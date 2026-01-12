f5cb628061863089d4a2a4866f4398b46564b830
Frank KOSSI <frank.kossi@sprint-pay.com>
2026-01-12 17:03:22 +0100

    fix: spring boot autoconfiguration

--- adapters/java/filterql-spring/src/main/java/io/github/cyfko/filterql/spring/autoconfigure/FilterQlAutoConfiguration.java
+++ adapters/java/filterql-spring/src/main/java/io/github/cyfko/filterql/spring/autoconfigure/FilterQlAutoConfiguration.java
@@ -2,18 +2,25 @@ package io.github.cyfko.filterql.spring.autoconfigure;
 
 import io.github.cyfko.filterql.jpa.JpaFilterContext;
 import io.github.cyfko.filterql.spring.support.FilterContextRegistry;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
-@Configuration
+@AutoConfiguration
+@ComponentScan(basePackages = "io.github.cyfko.filterql.spring")
 @ConditionalOnClass(JpaFilterContext.class)
 @EnableConfigurationProperties(FilterQlProperties.class)
 public class FilterQlAutoConfiguration {
+
     @Bean
+    @ConditionalOnMissingBean
     public FilterContextRegistry filterContextRegistry(List<JpaFilterContext<?>> contexts) {
         return new FilterContextRegistry(contexts);
     }
+
 }
