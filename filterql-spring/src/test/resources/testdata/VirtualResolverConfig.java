de2b8a9797bbb356248e3241aadb7309f360d57b
Frank KOSSI <frank.kossi@sprint-pay.com>
2026-02-10 02:47:33 +0100

    refactor: rearrange folders to simplify navigation

---
+++ filterql-spring/src/test/resources/testdata/VirtualResolverConfig.java
@@ -0,0 +1,31 @@
+package io.github.cyfko.example;
+
+import io.github.cyfko.filterql.jpa.spi.PredicateResolver;
+import io.github.cyfko.filterql.core.api.Op;
+import io.github.cyfko.filterql.spring.ExposedAs;
+
+/**
+ * VirtualResolverConfig - Defines static virtual field resolvers for Person entity
+ */
+public class VirtualResolverConfig {
+
+    /**
+     * Virtual field to filter admin users
+     */
+    @ExposedAs(
+            value = "IS_ADMIN",
+            operators = {Op.EQ}
+    )
+    public static PredicateResolver<Person> isAdminUser(String op, Object[] args) {
+        return (root, query, cb) -> {
+            Boolean isAdmin = args.length > 0 ? (Boolean) args[0] : false;
+            if (Boolean.TRUE.equals(isAdmin)) {
+                // Filter for admin users
+                return cb.equal(root.get("username"), "admin");
+            } else {
+                // Filter for non-admin users
+                return cb.notEqual(root.get("username"), "admin");
+            }
+        };
+    }
+}
